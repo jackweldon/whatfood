cbf9a349f06908b0a10d1380003cce572647b31e
jackw <jackw@S000657>
2017-11-24 13:12:59 +0000

    update readme

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -5,8 +5,8 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class RecipeService {
-  appKey: string = '';
-  appId: string = '';
+  appKey: string = 'a48c40199b1e65b95df0d2c87d3aa2dd';
+  appId: string = 'fbb6c485';
   private readonly serviceEndpoint = 'https://api.edamam.com/search?app_id=' + this.appId + '&app_key=' + this.appKey;
   constructor(private http: Http) { }
 
